da1a032709ce6c4e1b1d5bec36e25939810c1902
aslan.gama <aslan.gama@sixchains.com.br>
2026-03-20 15:58:15 -0300

    refactor(types): Update 2fa.ts type import path

--- src/runtime/passport/server/api/2fa.ts
+++ src/runtime/passport/server/api/2fa.ts
@@ -2,7 +2,7 @@ import { useRuntimeConfig } from '#imports';
 import { defineEventHandler, readBody, getCookie, setCookie, createError, setHeader } from 'h3';
 import { $fetch } from 'ofetch';
 import protectedMiddleware from '../middleware/protected';
-import type { PassportModuleOptions, PassportStrategiesOptions } from '../../../types';
+import type { PassportModuleOptions, PassportStrategiesOptions } from '#auth-types';
 
 interface Get2FAResponse {
   access_token: string;
